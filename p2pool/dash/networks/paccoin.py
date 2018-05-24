a8e5d337443072aa7cbb2292bee0d4e231f2bde7
kr1z1s <tormentor2000@gmail.com>
2018-05-24 09:31:30 +0300

    Init

---
+++ p2pool/dash/networks/paccoin.py
@@ -0,0 +1,28 @@
+import os
+import platform
+
+from twisted.internet import defer
+
+from .. import data, helper
+from p2pool.util import pack
+
+
+P2P_PREFIX = 'c8e5612c'.decode('hex')
+P2P_PORT = 7112
+ADDRESS_VERSION = 55
+SCRIPT_ADDRESS_VERSION = 10
+RPC_PORT = 7111
+RPC_CHECK = defer.inlineCallbacks(lambda dashd: defer.returnValue(
+            'paccoinaddress' in (yield dashd.rpc_help()) and
+            not (yield dashd.rpc_getinfo())['testnet']
+        ))
+BLOCKHASH_FUNC = lambda data: pack.IntType(256).unpack(__import__('dash_hash').getPoWHash(data))
+POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('dash_hash').getPoWHash(data))
+BLOCK_PERIOD = 150 # s
+SYMBOL = 'PAC'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'PaccoinCore') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/PaccoinCore/') if platform.system() == 'Darwin' else os.path.expanduser('~/.paccoincore'), 'dash.conf')
+BLOCK_EXPLORER_URL_PREFIX = 'http://eu.pacblockexplorer.com:3002/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'http://eu.pacblockexplorer.com:3002/address/'
+TX_EXPLORER_URL_PREFIX = 'http://eu.pacblockexplorer.com:3002/tx/'
+SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
+DUST_THRESHOLD = 0.001e8
---
+++ p2pool/networks/paccoin.py
@@ -0,0 +1,19 @@
+from p2pool.dash import networks
+
+PARENT = networks.nets['paccoin']
+SHARE_PERIOD = 20 # seconds
+CHAIN_LENGTH = 24*60*60//20 # shares
+REAL_CHAIN_LENGTH = 24*60*60//20 # shares
+TARGET_LOOKBEHIND = 100 # shares  //with that the pools share diff is adjusting faster, important if huge hashing power comes to the pool
+SPREAD = 10 # blocks
+IDENTIFIER = '1C018FE4180BA411'.decode('hex')
+PREFIX = '1C018FE1C3FFC012'.decode('hex')
+COINBASEEXT = '2f5032506f6f6c2d5041432f'.decode('hex')
+P2P_PORT = 2967
+MIN_TARGET = 0
+MAX_TARGET = 2**256//2**20 - 1
+PERSIST = True
+WORKER_PORT = 2968
+BOOTSTRAP_ADDRS = 'p2p-spb.xyz '.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool-pac'
+VERSION_CHECK = lambda v: v >= 120100
